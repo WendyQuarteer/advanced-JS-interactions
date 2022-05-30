2477e98cc42727fb9e03d66aafe1f4fcd57bcb07
Wendy Quarteer <wendyquarteer@outlook.com>
2022-05-30 15:37:59 +0200

    image slider works

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# advanced-JS-interactions
+# Advanced-JS-Interactions
+
+- Repository: `Advanced-JS-Interactions`
+- Type of Challenge: `Learning Challenge`
+- Duration: `2 days`
+- Start: 30/05/2022
+- Deployment strategy : 
+    - Github page
+- Team challenge : `solo`
+
+
+## The Mission
+Go check out the HTML file! Everything you need to know is there!
+
+## Must-have features
+- [x] add pictures for carousel slider.
+- [x] make images slide.
+- [] A parallax carousel
+- [] A collage with popup for more details
+- [] Hover image above every Pokemon in the text
+- [] Functional chaser or runner circle
+
+## Nice-to-have features
+- Chaser AND runner circle are functional
+- Lightmode / Darkmode switch
+- Final letter challenge completed!
+
---
+++ script.js
@@ -0,0 +1,27 @@
+const carousel = document.getElementsByClassName("carouselContainer");//location for paralax-carousel
+const carouselImg = [ //imgs to use for parallax carousel.
+    "images/carousel1.jpg",
+    "images/carousel2.jpg",
+    "images/carousel3.jpg",
+]
+let slideIndex = 0;//set variable for each time images change.
+function slide() {//set function to change images.
+    carousel[0].style.backgroundImage = "url(" + carouselImg[slideIndex] + ")";//change the blue background-image into the images.
+    slideIndex += 1;//each time picture changes slideIndex add 1.
+    if (slideIndex >= carouselImg.length) {//if slideIndex at the end of images it restarts at 0.
+        slideIndex = 0;
+    }
+}
+setInterval(slide, 2000);//images will change every 2 seconds.
+
+
+
+
+//Array.from(document.querySelectorAll(".letter")).forEach(el => {
+//el.innerText = randomLetter();
+//});
+
+//function randomLetter(){
+//const alphabet = "abcdefghijklmnopqrstuvwxyz"
+//return alphabet[Math.floor(Math.random() * alphabet.length)]
+//}
